53f6d5dd9ae1dc69a3c4460ea39a90cc7706d8de
Bardo91 <19Bardo91@gmail.com>
2020-03-15 21:12:46 +0100

    enabling video widget

--- run_gazebo.sh
+++ run_gazebo.sh
@@ -8,6 +8,10 @@ sed -i '17i#define TRUE true\n/' Tools/sitl_gazebo/include/gazebo_opticalflow_pl
 sed -i '26iinclude_directories(/usr/include/gstreamer-1.0 )\n' Tools/sitl_gazebo/CMakeLists.txt
 sed -i '26iinclude_directories(/usr/include/glib-2.0)\n' Tools/sitl_gazebo/CMakeLists.txt
 sed -i '26iinclude_directories(/usr/lib/x86_64-linux-gnu/glib-2.0/include)\n' Tools/sitl_gazebo/CMakeLists.txt
+sed -i '7i<gui>\n' Tools/sitl_gazebo/worlds/typhoon_h480.world
+sed -i '8i<plugin name="video_widget" filename="libgazebo_video_stream_widget.so"/>\n' Tools/sitl_gazebo/worlds/typhoon_h480.world
+sed -i '9i</gui>\n' Tools/sitl_gazebo/worlds/typhoon_h480.world
+
 
 make px4_sitl gazebo
 
